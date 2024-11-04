bbcc674c97bc3c5992f65e6b2f7d619c7ad32ade
EddyBough <boughanmieddy@outlook.fr>
2024-11-04 11:28:18 +0100

    terms page created and integrated and items added in navbar

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
+import { Terms } from "@/components/Terms";
 import { WaveBackground } from "@/components/WaveBackground";
-import { Privacy } from "@/components/Privacy";
 
-export default function PrivacyPage() {
+export default function TermsOfUsePage() {
   return (
     <div className="min-h-screen w-full overflow-y-auto">
       <WaveBackground />
       <div className="relative z-10 min-h-screen w-full">
         <div className="container mx-auto px-4 py-12 pb-32">
-          <Privacy />
+          <Terms />
         </div>
       </div>
     </div>
